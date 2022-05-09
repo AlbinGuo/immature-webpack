55d8f8b6fb2327f0696aea49aaf8292ee96525b8
guonan <838093891@qq.com>
2022-05-10 00:38:21 +0800

    add: url-loader

--- webpack.config.js
+++ webpack.config.js
@@ -8,11 +8,13 @@ module.exports = {
   module: {
     rules: [{
       test: /\.(png|jpg|gif)$/, // 打包的文件是以png结尾的
-      use: [{         // 使用loader打包图片
-        loader: 'file-loader',
+      use: [{                   // 使用loader打包图片
+        //loader: 'file-loader',  // file-loader是一个内置的loader
+        loader: 'url-loader',   // url-loader会把图片转成base64的字符串放入js文件中
         options: {
           name: '[name]-[hash].[ext]',    // placeholder占位符
-          outputPath: 'images/',             // 打包后的图片存放的路径
+          outputPath: 'images/',          // 打包后的图片存放的路径
+          limit: 10240                   // 图片大小小于10k就会转成base64放入bundle.js, 大于10k就会打包到images文件夹下
         }
       }]
     }]
